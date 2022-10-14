def2948bbad0fa4a0c64e0d44832637c9de473f9
Rytis <r.pleckauskas@gmail.com>
2022-10-14 16:04:25 +0300

    Commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,24 @@
+// import { Component } from "react";
+import './search-box.styles.css';
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+    return <input
+        className={`search-box ${className}`}
+        type="search"
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+    />
+}
+
+// class SearchBox extends Component {
+//     render() {
+//         return <input
+//             className={`search-box ${this.props.className}`}
+//             type="search"
+//             placeholder={this.props.placeholder}
+//             onChange={this.props.onChangeHandler}
+//         />
+//     }
+// }
+
+export default SearchBox;
